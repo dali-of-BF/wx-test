7f281630f976ac7ac5deda38c01fcd9b98c1a8e6
fang <1574624619@qq.com>
2022-08-22 23:18:11 +0800

    feat:联调微信测试号，引入weixin4j-spring-boot-starter依赖

---
+++ src/main/java/com/fang/wxcloudrun/controller/WxController.java
@@ -0,0 +1,21 @@
+package com.fang.wxcloudrun.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.weixin4j.spring.web.WeixinJieruController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author FPH
+ * @since 2022年8月22日22:46:10
+ */
+@RestController
+@RequestMapping("load")
+public class WxController extends WeixinJieruController {
+
+    @Override
+    public String getToken(HttpServletRequest httpServletRequest) {
+        return null;
+    }
+}
