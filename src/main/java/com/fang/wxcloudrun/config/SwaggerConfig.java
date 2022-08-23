b09a9f8f33af3f8b8cfdd44ca786352807aa4974
方鹏辉 <fangpenghui@one-sports.com>
2022-08-23 09:52:21 +0800

    feat:新增swagger测试 fix:修改微信控制类为@Controller

---
+++ src/main/java/com/fang/wxcloudrun/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.fang.wxcloudrun.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author FPH
+ * @since 2022年8月23日09:45:17
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .build();
+    }
+    /**
+     * 创建该API的基本信息（这些基本信息会展现在文档页面中）
+     * 访问地址：http://项目实际地址/swagger-ui.html
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("微信测试框架")
+                .description("")
+                .termsOfServiceUrl("https://gitee.com/durank/wx_test")
+                .version("1.0")
+                .build();
+    }
+}
--- src/main/java/com/fang/wxcloudrun/controller/WxController.java
+++ src/main/java/com/fang/wxcloudrun/controller/WxController.java
@@ -1,16 +1,14 @@
 package com.fang.wxcloudrun.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.weixin4j.spring.web.WeixinJieruController;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author FPH
  * @since 2022年8月22日22:46:10
  */
-@RestController
+@Controller
 @RequestMapping("load")
 public class WxController extends WeixinJieruController {
 
